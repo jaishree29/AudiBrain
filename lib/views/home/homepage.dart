877b070815ce37565464599d4382da3317eedc5a
Jaishree <jaishreeofficial29@gmail.com>
2025-02-16 00:12:25 +0530

    Added bluetooth pairing

--- lib/views/home/homepage.dart
+++ lib/views/home/homepage.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:audibrain/utils/colors.dart';
-import 'package:audibrain/utils/image_strings.dart';
 import 'package:audibrain/views/home/app_drawer.dart';
 import 'package:audibrain/views/notifications/notifications.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_classic/flutter_blue_classic.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,14 +20,27 @@ class _HomePageState extends State<HomePage>
   late AnimationController _controller;
   late Animation<Offset> _animation;
   bool _isDrawerOpen = false;
+  final _flutterBlueClassicPlugin = FlutterBlueClassic();
 
-  // List of supported languages
-  final List<String> _languages = ['English', 'Spanish', 'French', 'German'];
-  String _selectedLanguage = 'English';
+  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+  StreamSubscription? _adapterStateSubscription;
+
+  final Set<BluetoothDevice> _scanResults = {};
+  StreamSubscription? _scanSubscription;
+
+  bool _isScanning = false;
+  int? _connectingToIndex;
+  StreamSubscription? _scanningStateSubscription;
+
+  // // List of supported languages
+  // final List<String> _languages = ['English', 'Spanish', 'French', 'German'];
+  // String _selectedLanguage = 'English';
 
   @override
   void initState() {
     super.initState();
+    _requestPermissions();
+    _initBluetooth();
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 300),
@@ -40,9 +56,55 @@ class _HomePageState extends State<HomePage>
     );
   }
 
+  Future<void> _requestPermissions() async {
+    await [
+      Permission.bluetooth,
+      Permission.bluetoothScan,
+      Permission.bluetoothConnect,
+      Permission.locationWhenInUse,
+    ].request();
+  }
+
+  Future<void> _initBluetooth() async {
+    await _requestPermissions();
+    await _checkBluetoothState();
+
+    try {
+      _adapterState = await _flutterBlueClassicPlugin.adapterStateNow;
+      _adapterStateSubscription =
+          _flutterBlueClassicPlugin.adapterState.listen((current) {
+        if (mounted) setState(() => _adapterState = current);
+      });
+      _scanSubscription =
+          _flutterBlueClassicPlugin.scanResults.listen((device) {
+        if (mounted) setState(() => _scanResults.add(device));
+      });
+      _scanningStateSubscription =
+          _flutterBlueClassicPlugin.isScanning.listen((isScanning) {
+        if (mounted) setState(() => _isScanning = isScanning);
+      });
+    } catch (e) {
+      print("Bluetooth initialization error: $e");
+    }
+  }
+
+  Future<void> _checkBluetoothState() async {
+    BluetoothAdapterState adapterState =
+        await _flutterBlueClassicPlugin.adapterStateNow;
+
+    if (adapterState != BluetoothAdapterState.on) {
+      _flutterBlueClassicPlugin.turnOn();
+      await Future.delayed(const Duration(seconds: 2));
+    }
+  }
+
   @override
   void dispose() {
+    _adapterStateSubscription?.cancel();
+    _scanSubscription?.cancel();
+    _scanningStateSubscription?.cancel();
     _controller.dispose();
+    _adapterState;
     super.dispose();
   }
 
@@ -57,20 +119,21 @@ class _HomePageState extends State<HomePage>
     });
   }
 
-  // Function to handle language change
-  void _onLanguageChanged(String? newValue) {
-    if (newValue != null) {
-      setState(() {
-        _selectedLanguage = newValue;
-      });
-      // Here you would typically call a function to update the app's language
-      // For example: updateAppLanguage(newValue);
-    }
-  }
+  // // Function to handle language change
+  // void _onLanguageChanged(String? newValue) {
+  //   if (newValue != null) {
+  //     setState(() {
+  //       _selectedLanguage = newValue;
+  //     });
+  //     // Here you would typically call a function to update the app's language
+  //     // For example: updateAppLanguage(newValue);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+    List<BluetoothDevice> scanResults = _scanResults.toList();
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 80,
@@ -221,6 +284,54 @@ class _HomePageState extends State<HomePage>
                 const SizedBox(
                   height: 20,
                 ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_isScanning) {
+                      _flutterBlueClassicPlugin.stopScan();
+                    } else {
+                      _scanResults.clear();
+                      _flutterBlueClassicPlugin.startScan();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AColors.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    foregroundColor: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 60.0, vertical: 10),
+                    child: Text(
+                      _isScanning ? 'Scanning...' : 'Scan for devices',
+                      style: TextStyle(
+                        fontFamily: 'Canva Sans',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Available devices:',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Canva Sans',
+                        color: AColors.primary,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                _buildDeviceList(scanResults),
+                const SizedBox(height: 30),
               ],
             ),
             Visibility(
@@ -235,6 +346,82 @@ class _HomePageState extends State<HomePage>
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildDeviceList(List<BluetoothDevice> scanResults) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (scanResults.isEmpty)
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: AColors.primary.withOpacity(0.1),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.27,
+                    vertical: 20,
+                  ),
+                  child: Text(
+                    "No devices found yet",
+                    style: TextStyle(
+                      fontFamily: 'Canva Sans',
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          else
+            for (var (index, result) in scanResults.indexed)
+              Card(
+                color: AColors.primary.withOpacity(0.1),
+                elevation: 0,
+                margin: const EdgeInsets.symmetric(vertical: 5),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(12),
+                  title: Text(
+                    "${result.name ?? "???"} (${result.address})",
+                    style: TextStyle(
+                      fontFamily: 'Canva Sans',
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                      "Bondstate: ${result.bondState.name}, Device type: ${result.type.name}"),
+                  trailing: index == _connectingToIndex
+                      ? const CircularProgressIndicator()
+                      : Text("${result.rssi} dBm"),
+                  onTap: () async {
+                    setState(() => _connectingToIndex = index);
+                    try {
+                      final connection =
+                          await _flutterBlueClassicPlugin.connect(
+                        result.address,
+                      );
+                      if (connection!.isConnected) {
+                        setState(() => _connectingToIndex = null);
+                      }
+                    } catch (e) {
+                      setState(() => _connectingToIndex = null);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Error connecting: $e")),
+                      );
+                    }
+                  },
+                ),
+              ),
+        ],
       ),
     );
   }
